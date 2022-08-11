d753eaf016cab61730dc59e8653f1e0af10fabce
Alejandro Fonseca <alejofon04@gmail.com>
2022-08-10 20:30:59 -0500

    First breakthroung

---
+++ Amiibopedia/Amiibopedia/ViewModel/BaseViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Amiibopedia.ViewModel
+{
+    internal class BaseViewModel
+    {
+    }
+}
---
+++ Amiibopedia/Amiibopedia/ViewModel/MainPageViewModel.cs
@@ -0,0 +1,23 @@
+﻿using Amiibopedia.Helpers;
+using Amiibopedia.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Amiibopedia.ViewModel
+{
+    internal class MainPageViewModel : BaseViewModel
+    {
+        public ObservableCollection<Character> Characters { get; set; }
+
+        public async Task LoadCharacters()
+        {
+            var url = "";
+            var service = new HttpHelper<Characters>();
+            var characters = await service.GetRestServiceDataAsync(url);
+            Characters = new ObservableCollection<Character>(characters.amiibo);
+        }
+    }
+}
